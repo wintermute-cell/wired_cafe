50609c8c1b68d795f31e45fd32fd27851b79ee8f
wintermute-cell <kjulius@gmx.de>
2022-07-21 10:35:43 +0200

    x

---
+++ src/config.ts
@@ -0,0 +1,2 @@
+//export const IP_ADDR: string = 'http://78.141.213.57:8080'
+export const IP_ADDR: string = 'http://localhost:8080'
--- src/view/Clientpage.svelte
+++ src/view/Clientpage.svelte
@@ -1,8 +1,72 @@
 <script lang="ts">
+  import {onMount} from 'svelte';
+  import {IP_ADDR} from '../config';
+
+  export let gotoFrontpage;
+  export let gotoClientpage;
+  export let gotoReceptionist;
+
+  // the following interfaces/arrays hold information about the room.
+  interface Message {
+    sender: string;
+    content: string;
+  }
+  let messages: Array<Message> = [];
+
+  interface User {
+    name: string;
+  }
+  let users: Array<User> = [];
+
+  onMount(async () => {
+    // fetch initial data from the server
+    fetch(IP_ADDR + '/hello', {credentials:'include'}).then(() => {
+      fetch(IP_ADDR + '/room/join', {credentials:'include'})
+        .then(response => response.json())
+        .then(data => {
+          if (data['message'] === 'Success') {
+            const frii = setInterval(fetchRoomInfo, 30000);
+            fetchRoomInfo();
+            return () => clearInterval(frii);
+          } else {
+            console.log(data)
+          }
+        })
+        .catch(error => {
+          console.log(error);
+          return [];
+        });
+    }).catch(error => {
+      console.log(error);
+      return [];
+    });
+
+    // start periodical fetching of room info
+    async function fetchRoomInfo() {
+    fetch(IP_ADDR + '/room/info', {credentials:'include'})
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+      })
+      .catch(error => {
+        console.log(error);
+        return [];
+      });
+    }
+  });
 </script>
 
 <main>
-  Cafe
+  <div id="user-list">
+    {#each users as user}
+      {user.name}
+    {/each}
+  </div>
+  <div id="message-log">
+    {#each messages as msg}
+      {msg.content}
+    {/each}
+  </div>
 </main>
 
 <style>
