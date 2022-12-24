36d9795af8ac1a6e085e58f23d79a2fcff6b5597
wintermute <kjulius@gmx.de>
2022-12-24 04:28:30 +0100

    test

--- gitpush.sh
+++ gitpush.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-git add .; git commit -m"$1"; git push
 cd frontend
 git add .; git commit -m"$1"; git push
 cd ../backend
+git add .; git commit -m"$1"; git push
+cd ..
 git add .; git commit -m"$1"; git push
